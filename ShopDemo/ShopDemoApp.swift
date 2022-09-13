300cad0033b0e88fd21599cbab23a0e5e2081bee
Eitan Elberg <eitan.elberg@consumrz.com>
2022-09-13 21:29:57 +0300

    Initial Commit

---
+++ ShopDemo/ShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopDemoApp.swift
+//  ShopDemo
+//
+//  Created by Eitan Levi Berger on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
